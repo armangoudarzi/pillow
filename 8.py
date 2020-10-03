9eadf84d1161a862229e01ab74abf3e949b49891
armangoudarzi <71977884+armangoudarzi@users.noreply.github.com>
2020-10-03 09:03:14 +0330

    Update 8.py

--- 8.py
+++ 8.py
@@ -24,7 +24,51 @@ def dsl():
             print("1-Insert new pic")
             print("2-Export new file")
             print("3-Edit")
-            print("4-rotate")
+            print("4-rotate")import functions
+from PIL import Image,ImageOps
+img=None
+user_exit=True
+_input_=functions.main_menu()
+while user_exit:
+    while _input_[0]!="exit":
+        img=functions.insert_menu(_input_) 
+        while 4>=len(_input_)>=3 and _input_[1]=="edit":
+            img=functions.edit_menu(img,_input_)
+            _input_=functions.main_menu()
+            if _input_[0]=="exit":
+                print("Thanks for using my code")
+                user_exit=False
+        while 4>=len(_input_)>=3 and _input_[1]=="rotate":
+            img=functions.rotate_menu(img,_input_)
+            _input_=functions.main_menu()
+            if _input_[0]=="exit":
+                print("Thanks for using my code")
+                user_exit=False    
+        while 4>=len(_input_)>=3 and _input_[1]=="resize":
+            img=functions.resize_menu(img,_input_)
+            _input_=functions.main_menu()
+            if _input_[0]=="exit":
+                print("Thanks for using my code")
+                user_exit=False
+        while 4>=len(_input_)>=3 and _input_[1]=="crop":
+            img=functions.crop_menu(img,_input_)
+            _input_=functions.main_menu()
+            if _input_[0]=="exit":
+                print("Thanks for using my code")
+                user_exit=False
+        while 3>=len(_input_)>=2 and _input_[1]=="Horizontally_Flipped":
+            img=functions.Horizontally_Flipped(img,_input_)
+            _input_=functions.main_menu()
+            if _input_[0]=="exit":
+                print("Thanks for using my code")
+                user_exit=False
+        while 3>=len(_input_)>=2 and _input_[1]=="Vertically_Flipped":
+            img=functions.Vertically_Flipped(img,_input_)
+            _input_=functions.main_menu()
+            if _input_[0]=="exit":
+                print("Thanks for using my code")
+                user_exit=False
+
             print("5-resize")
             print("6-crop")
             print("7-Horizontally Flipped")
